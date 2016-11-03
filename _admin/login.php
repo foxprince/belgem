d783d26e9dddffc6d5c26579334109c8186e3c24
zj-dell <9600348@qq.com>
2016-11-03 20:30:05 +0800

    login with disable

--- _admin/login.php
+++ _admin/login.php
@@ -1,56 +1,51 @@
-<?php 
-$error='';
-if(isset($_POST['login']) && isset($_POST['pwd'])){
-	//echo $_POST['login'];
-    session_start();
-	$username=$_POST['login'];
-	$password=$_POST['pwd'];
+<?php
+$error = '';
+if (isset ( $_POST ['login'] ) && isset ( $_POST ['pwd'] )) {
+	// echo $_POST['login'];
+	session_start ();
+	$username = $_POST ['login'];
+	$password = $_POST ['pwd'];
 	
-	require_once('../includes/connection.php');
-	$conn=dbConnect('write','pdo');
-	$conn->query("SET NAMES 'utf8'");
+	require_once ('../includes/connection.php');
+	$conn = dbConnect ( 'write', 'pdo' );
+	$conn->query ( "SET NAMES 'utf8'" );
 	
-	$sql='SELECT * FROM users WHERE user_name = "'.$username.'" AND pass_word = "'.$password.'"';
-	$stmt=$conn->query($sql);
-	$userexits=$stmt->rowCount();
+	$sql = 'SELECT * FROM users WHERE disable=0 and user_name = "' . $username . '" AND pass_word = "' . $password . '"';
+	$stmt = $conn->query ( $sql );
+	$userexits = $stmt->rowCount ();
 	
-	//exit($userexits);
+	// exit($userexits);
 	
-	if(!$userexits){
-		$_SESSION=array();
-		if (isset($_COOKIE[session_name()])){
-			setcookie(session_name(), '', time()-86400, '/');
+	if (! $userexits) {
+		$_SESSION = array ();
+		if (isset ( $_COOKIE [session_name ()] )) {
+			setcookie ( session_name (), '', time () - 86400, '/' );
 		}
-		session_destroy();
-		$wrongmessage='用户名密码不正确，请重试';
-	}else{
-		$_SESSION['authenticated']='SiHui';
-		session_regenerate_id();		
+		session_destroy ();
+		$wrongmessage = '用户名密码不正确，请重试';
+	} else {
+		$_SESSION ['authenticated'] = 'SiHui';
+		session_regenerate_id ();
 		
-		
-		foreach($stmt as $row_account){
-			$account_level=$row_account['account_level'];
+		foreach ( $stmt as $row_account ) {
+			$account_level = $row_account ['account_level'];
 		}
-		$_SESSION['username']=$username;
-		$_SESSION['account_level']=$account_level;
+		$_SESSION ['username'] = $username;
+		$_SESSION ['account_level'] = $account_level;
 		
-		$the_action='登陆';
+		$the_action = '登陆';
 		
-		$sql_record_login='INSERT INTO login_history (theuser, the_action, action_time) VALUES(:theuser, :the_action, NOW())';
+		$sql_record_login = 'INSERT INTO login_history (theuser, the_action, action_time) VALUES(:theuser, :the_action, NOW())';
 		
+		$stmt = $conn->prepare ( $sql_record_login );
+		$stmt->bindParam ( ':theuser', $username, PDO::PARAM_STR );
+		$stmt->bindParam ( ':the_action', $the_action, PDO::PARAM_STR );
+		$stmt->execute ();
+		$insertOK = $stmt->rowCount ();
 		
-		$stmt=$conn->prepare($sql_record_login);	  
-		$stmt->bindParam(':theuser', $username, PDO::PARAM_STR);
-		$stmt->bindParam(':the_action', $the_action, PDO::PARAM_STR);
-		$stmt->execute();
-		$insertOK=$stmt->rowCount();
-		
-		header('Location: index.php');
-		exit('');
+		header ( 'Location: index.php' );
+		exit ( '' );
 	}
-	
-	
-	
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -58,66 +53,77 @@ if(isset($_POST['login']) && isset($_POST['pwd'])){
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <style>
-body{
-	font-family:Georgia, "Times New Roman", Times, serif;
-	font-size:14px;
-	font-weight:100;
-	padding:0;
-	margin:0;
-	border-top-style:solid;
-	border-width:5px;
-	border-color:#333;
-	background-color:#333;
-	}
-img{
-	width:300px;
-	position:relative;
+body {
+	font-family: Georgia, "Times New Roman", Times, serif;
+	font-size: 14px;
+	font-weight: 100;
+	padding: 0;
+	margin: 0;
+	border-top-style: solid;
+	border-width: 5px;
+	border-color: #333;
+	background-color: #333;
 }
-h1{
-	position:relative;
-	font-family:Verdana, Geneva, sans-serif;
-	font-weight:bold;
-	font-size:20px;
-	color:#000;
-	margin-top:0px;
-}
-form{
-	position:relative;
-	}
 
-p{
-	margin-top:30px;
-	}
-label{
-	}
-.formbox{
-	width:450px;}
-.alert{
-	font-family:"Courier New", Courier, monospace;
-	font-size:14px;
-	color:#F00;
-	position:relative;
-	left:40px;}
-span{
-	color:#F00;
-	}
-.warning{
-	position:relative;
-	font-family:"Courier New", Courier, monospace;
-	color:#F00;
-	text-align:center;
+img {
+	width: 300px;
+	position: relative;
 }
-input{
-	font-size:16px;
-	padding:8px 50px;
-	color:#FFF;
-	background-color:#666;
-	border-style:none;
-	border-width:0;
-	text-align:center;
+
+h1 {
+	position: relative;
+	font-family: Verdana, Geneva, sans-serif;
+	font-weight: bold;
+	font-size: 20px;
+	color: #000;
+	margin-top: 0px;
+}
+
+form {
+	position: relative;
+}
+
+p {
+	margin-top: 30px;
+}
+
+label {
+	
+}
+
+.formbox {
+	width: 450px;
+}
+
+.alert {
+	font-family: "Courier New", Courier, monospace;
+	font-size: 14px;
+	color: #F00;
+	position: relative;
+	left: 40px;
+}
+
+span {
+	color: #F00;
+}
+
+.warning {
+	position: relative;
+	font-family: "Courier New", Courier, monospace;
+	color: #F00;
+	text-align: center;
+}
+
+input {
+	font-size: 16px;
+	padding: 8px 50px;
+	color: #FFF;
+	background-color: #666;
+	border-style: none;
+	border-width: 0;
+	text-align: center;
 	margin-bottom: 5px;
 }
-
 </style>
 <title>BELGEM: login</title>
 <!-- plugin placeholder -->
@@ -307,43 +313,38 @@ input{
 		} catch (err) {}
 	}
 
-}(this, document, jQuery));  
+}(this, document, jQuery));
 </script>
 <!-- end of the placeholder plugin -->
 <!-- end of the placeholder plugin -->
 <!-- end of the placeholder plugin -->
 </head>
-
 <body>
-
-
-<p style='text-align:center;'><img src="../images/belgemlogo.png" style="width:128px;" /></p>
-<h1 style="margin-top:20px; text-align:center; color:#FFF;">比利时BELGEM钻石公司<br />国际钻石交易平台</h1>
+  <p style='text-align: center;'>
+    <img src="../images/belgemlogo.png" style="width: 128px;" />
+  </p>
+  <h1 style="margin-top: 20px; text-align: center; color: #FFF;">
+    比利时BELGEM钻石公司<br />国际钻石交易平台
+  </h1>
 <?php
-if(isset($wrongmessage)){
-    echo "<p class='warning'>$wrongmessage</p>";
+if (isset ( $wrongmessage )) {
+	echo "<p class='warning'>$wrongmessage</p>";
 }
 ?>
 
 <form id="loginform" method="post" action="">
+    <p style="text-align: center;">
+      <input type="text" name="login" id="login" placeholder="用户名"><br /> <input type="password" name="pwd" id="pwd"
+        placeholder="密码"><br /> <input name="denglu" type="submit" id="login" value="登陆"
+          style="background-color: #000; margin-top: 55px;">
     
-    <p style="text-align:center;">
-        
-        <input type="text" name="login" id="login" placeholder="用户名"><br />
-        
-        <input type="password" name="pwd" id="pwd" placeholder="密码"><br />
-        <input name="denglu" type="submit" id="login" value="登陆" style="background-color:#000; margin-top:55px;">
     </p>
-    
-        
-</form>
-
-<script>
+  </form>
+  <script>
 $(function() {
 // Invoke the plugin
 	$('input, textarea').placeholder();
 });
 </script>
-
 </body>
 </html>
