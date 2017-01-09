2f74bc9d25f99656b9b74baa0b0acc44402e9c23
foxprince <9600348@qq.com>
2017-01-09 22:03:08 +0800

    mysql case

--- _admin/login.php
+++ _admin/login.php
@@ -10,7 +10,7 @@ if (isset ( $_POST ['login'] ) && isset ( $_POST ['pwd'] )) {
 	$conn = dbConnect ( 'write', 'pdo' );
 	$conn->query ( "SET NAMES 'utf8'" );
 	
-	$sql = 'SELECT * FROM users WHERE disable=0 and user_name = "' . $username . '" AND pass_word = "' . $password . '"';
+	$sql = 'SELECT * FROM users WHERE disable=0 and user_name = binary "' . $username . '" AND pass_word = binary "' . $password . '"';
 	$stmt = $conn->query ( $sql );
 	$userexits = $stmt->rowCount ();
 	
