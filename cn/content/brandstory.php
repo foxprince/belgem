87ffd0f716755cfb631fb375ea5f21d4af780694
foxprince <9600348@qq.com>
2017-03-26 20:22:16 +0800

    cc

--- cn/content/brandstory.php
+++ cn/content/brandstory.php
@@ -9,7 +9,9 @@
 <p><span style="font-size: 12pt;"><span style="font-size: 12pt;"></span></span></p>
 <p><span style="font-size: 12pt;">安特卫普市坐落在直通北海的斯海尔得河河畔，是比利时荷兰语区的重点城市。自19世纪初拿破伦下令在此建港，现在已成为欧洲第二大港口，也是比利时最重要的商业中心。</span></p>
 <p><br><span style="font-size: 12pt;">举世闻名的"钻石街"毗邻安特卫普中央火车站，全球27家钻石交易所中有4家就坐落于其中，是全世界最大的钻石交易中心。每年全球被开采的1.3亿克拉的钻石中，其中84%的原石和将近一半的成品钻石都会流经这里。</span></p>
-<p><br><span style="font-size: 12pt;">安特卫普的钻石企业共有约1800家。利美所属的Belgem钻石公司，是安特卫普钻交所成员中唯一一家由中国人创立的钻石公司。</span></p>
+<p><br><span style="font-size: 12pt;">安特卫普的钻石企业共有约1800家。<a href="http://www.lumiagem.com">利美钻石</a>所属的Belgem钻石公司，是安特卫普钻交所成员中唯一一家由中国人创立的钻石公司。</span></p>
+<p>利美钻石专注于<a href="http://www.lumiagem.com/cn/guide.php?p=knowledge" title="安特卫普钻石"><strong>安特卫普钻石</strong></a>和<a href="http://www.lumiagem.com/cn/guide.php?p=knowledge" title="比利时钻石">比利时钻石</a></p>
+<p>更多钻石请看这里:<a href="http://www.lumiagem.com/cn/guide.php?p=knowledge" title="钻石知识">钻石知识</a></p>
 </div>
 
 
