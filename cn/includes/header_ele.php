1b506809cc22139e4e93ed2b54810e544cc9a4c7
foxprince <9600348@qq.com>
2017-09-12 06:59:08 +0800

    dfsdfdf

--- cn/includes/header_ele.php
+++ cn/includes/header_ele.php
@@ -1,20 +1,14 @@
 <?php
-if(isset($crr_page) && $crr_page=='jewelry'){
 	require_once('../includes/connection.php');
 	$conn=dbConnect('write','pdo');
 	$conn->query("SET NAMES 'utf8'");
-}else{
-	require_once('../includes/connection.php');
-	$conn=dbConnect('write','pdo');
-	$conn->query("SET NAMES 'utf8'");
-}
 
 ?>
-
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<meta content="比利时钻石,安特卫普钻石,钻石购买,钻交所,钻石交易所，比利时钻交所，比利时钻石交易所" name="keywords" />
+<meta name="description" content="利美钻石主营产品：比利时钻石，安特卫普钻石，利美钻石婚戒定制专家，为您专业定制独一无二的钻戒，每颗钻石都配有国际钻石证书，清晰的解释，贴心的建议和优惠的价格是利美对您的承诺，作为华人的购钻渠道，利美钻石将竭诚为您的选择提供最专业的服务。"/>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta content="BELGEM" name="description">
 <link href="../styles/main.css?v=<?php echo strtotime('now'); ?>" media="screen" rel="stylesheet" type="text/css" />
@@ -27,12 +21,10 @@ if(isset($crr_page) && $crr_page=='jewelry'){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
   ga('create', 'UA-84729490-2', 'auto');
   ga('send', 'pageview');
-
 </script>
-<script>
+<script></script>
 <!--百度seo自动推送 -->
 (function(){
     var bp = document.createElement('script');
