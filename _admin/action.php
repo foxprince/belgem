3050b645980e257af0408cf7ad1bdb8eb3366258
foxprince <9600348@qq.com>
2017-09-18 18:25:40 +0800

    fdsf

--- _admin/action.php
+++ _admin/action.php
@@ -307,72 +307,137 @@ if($_REQUEST['action']) {
 			}
 			echo $t;
 			break;
-		case "invoiceList":
-			$totalSql = 'select count(*) as t from invoice';
-			$sql='select id,invoice_no,invoice_date,name,currency,vat_price,total_price from invoice';
+		case "trancDetail":
+			$stmt=$conn->prepare('select * from transaction where id=:id');
+			$stmt->execute(array('id'=>$_REQUEST['id']));
+			foreach($stmt as $r){
+				$transactionDetail=$r;
+			}
+			$stmt=$conn->prepare('select * from tranc_detail where tranc_id=:tranc_id');
+			$stmt->execute(array('tranc_id'=>$_REQUEST['id']));
+			$tranc_detailList=array();
+			foreach($stmt as $row){
+				$tranc_detailList[]=$row;
+			}
+			$result = array('trancDetail'=>$transactionDetail,'list'=>$tranc_detailList);
+			echo json_encode($result);
+			break;
+		case "trancList":
+			$totalSql = 'select count(*) as t from transaction';
+			$sql='select id,type,invoice_no,tranc_date,name,currency,vat_price,total_price from transaction ';
 			$clause = ' where 1=1 ';
+			if($_REQUEST['type']!=null)
+				$clause .= ' and type ="'.$_REQUEST['type'].'"';
 			if($_REQUEST['name']!=null)
 				$clause .= ' and name like "%'.$_REQUEST['name'].'%"';
 			if($_REQUEST['invoice_no']!=null)
 				$clause .= ' and invoice_no like "%'.$_REQUEST['invoice_no'].'%"';
 			if($_REQUEST['start']!=null)
-				$clause .= ' and invoice_date>='.$_REQUEST['start'];
+				$clause .= ' and tranc_date>='.$_REQUEST['start'];
 			if($_REQUEST['end']!=null)
-				$clause .= ' and invoice_date<'.$_REQUEST['end'];
+				$clause .= ' and tranc_date<'.$_REQUEST['end'];
 			$pagesize = 10;
-			if(isset($_REQUEST['page'])){
+			if(isset($_REQUEST['page'])&&$_REQUEST['page']!=null){
 				$crr_page=$_REQUEST['page'];
 			}else{
 				$crr_page=1;
 			}
 			$startfrom=($crr_page-1)*$pagesize;
 			$totalSql .= $clause;
-			$sql .= $clause.' limit '.$startfrom.','.$pagesize;logger($sql);
+			$sql .= $clause.' order by id desc limit '.$startfrom.','.$pagesize;
 			foreach($conn->query($totalSql) as $r_r){
 				$total=$r_r['t'];
 			}
 			if($total>0) {
-				$invoiceList=array();$i=0;
+				$transactionList=array();$i=0;
 				foreach($conn->query($sql) as $row){
-					$invoiceList[]=$row;
+					$transactionList[]=$row;
 					$i++;
 				}
 			}
 			$tpages = ceil ( $total / $pagesize );
-			$result = array('total'=>$total,'page'=>$crr_page,'total_pages'=>$tpages,'list'=>$invoiceList);
+			$result = array('total'=>$total,'page'=>$crr_page,'total_pages'=>$tpages,'list'=>$transactionList);
 			echo json_encode($result);
 			break;
-		case "receipt":
-			$obj=json_decode($_REQUEST['receipt'],TRUE);
-			$sql = 'insert into invoice(name,passport,street,city,postcode,country,invoice_date,invoice_no,currency,vat_price,total_price,ctime) 
-					values(:name,:passport,:street,:city,:postcode,:country,:invoice_date,:invoice_no,:currency,:vat_price,:total_price,now())';
+		case "updateTranc":
+			$obj=json_decode($_REQUEST['transaction'],TRUE);
+			$sql = 'update transaction set type=?,name=?,passport=?,street=?,city=?,postcode=?,country=?,tranc_date=?,invoice_no=?,currency=?,vat_price=?,total_price=? where id=?';
+			$stmt=$conn->prepare($sql);
+			$stmt->execute(array($obj['type'],$obj['name'],
+					$obj['passport'],
+					$obj['street'],$obj['city'],
+					$obj['postcode'],
+					$obj['country'],
+					$obj['tranc_date'],
+					$obj['invoice_no'],$obj['currency'],$obj['vat_price'],$obj['total_price'],$obj['id']));
+			//删除原有纪录
+			$sql_delete='delete from tranc_detail WHERE tranc_id = '.$obj['id'];
+			$conn->query($sql_delete);
+			$num=count($obj["list"]);
+			//--遍历数组，将对应信息添加入数据库
+			for ($i=0;$i<$num;$i++) {
+				$item = $obj["list"][$i];
+				if($item["type"]=='jew') {
+					$insert_sql="INSERT INTO tranc_detail (tranc_id,type,jewerly,material,jewerly_price,ctime)
+						VALUES (?,?,?,?,?,now())";
+					$result = $conn -> prepare($insert_sql);
+					$result -> execute(array( $obj['id'],$item["type"],$item["jewerly"], $item["material"],
+							$item["jewerly_price"]
+					));
+				}
+				else{
+					$insert_sql="INSERT INTO tranc_detail (tranc_id,type,report_no,shape,color,fancy,grading_lab,carat,
+						clarity,cut_grade,polish,symmetry,price,jewerly,material,jewerly_price,ctime)
+						VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,now())";
+					$result = $conn -> prepare($insert_sql);
+					$result -> execute(array( $obj['id'],$item["type"],$item["report_no"],$item["shape"],$item["color"],
+							$item["fancy"],$item["grading_lab"],$item["carat"], $item["clarity"],$item["cut_grade"],
+							$item["polish"], $item["symmetry"],$item["price"],$item["jewerly"], $item["material"],
+							$item["jewerly_price"]
+					));
+				}
+			}
+			echo $obj['id'];
+			break;
+		case "addTranc":
+			$obj=json_decode($_REQUEST['transaction'],TRUE);logger($_REQUEST['transaction']);
+			$sql = 'insert into transaction(name,passport,street,city,postcode,country,type,tranc_date,invoice_no,currency,vat_price,total_price,ctime) 
+					values(:name,:passport,:street,:city,:postcode,:country,:type,:tranc_date,:invoice_no,:currency,:vat_price,:total_price,now())';
 			$stmt=$conn->prepare($sql);
 			$stmt->execute(array('name'=>$obj['name'],
 					'passport'=>$obj['passport'],
 					'street'=>$obj['street'],'city'=>$obj['city'],
 					'postcode'=>$obj['postcode'],
-					'country'=>$obj['country'],
-					'invoice_date'=>$obj['invoice_date'],
+					'country'=>$obj['country'],'type'=>$obj['type'],
+					'tranc_date'=>$obj['tranc_date'],
 					'invoice_no'=>$obj['invoice_no'],'currency'=>$obj['currency'],'vat_price'=>$obj['vat_price'],'total_price'=>$obj['total_price']));
-			$invoiceId = $conn->lastInsertId();
+			$transactionId = $conn->lastInsertId();
 			//--得到Json_list数组长度
-			$num=count($obj["json_list"]);
+			$num=count($obj["list"]);
 			//--遍历数组，将对应信息添加入数据库
 			for ($i=0;$i<$num;$i++) {
-				$item = $obj["json_list"][$i];
-				$insert_order_product_sql="INSERT INTO receipt (invoice_id,report_no,shape,color,fancy,grading_lab,carat,
+				$item = $obj["list"][$i];
+				if($item["type"]=='jew') {
+					$insert_sql="INSERT INTO tranc_detail (tranc_id,type,jewerly,material,jewerly_price,ctime)
+						VALUES (?,?,?,?,?,now())";
+					$result = $conn -> prepare($insert_sql);
+					$result -> execute(array( $transactionId,$item["type"],$item["jewerly"], $item["material"],
+							$item["jewerly_price"]
+					));
+				}
+				else{
+					$insert_sql="INSERT INTO tranc_detail (tranc_id,type,report_no,shape,color,fancy,grading_lab,carat,
 						clarity,cut_grade,polish,symmetry,price,jewerly,material,jewerly_price,ctime)
-						VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,now())";
-				$result = $conn -> prepare($insert_order_product_sql);
-				$result -> execute(array( $invoiceId,$item["report_no"],$item["shape"],$item["color"],
-						$item["fancy"],$item["grading_lab"],$item["carat"],
-						$item["clarity"],$item["cut_grade"],$item["polish"],
-						$item["symmetry"],$item["price"],$item["jewerly"],
-						$item["material"],$item["jewerly_price"]
-				));
+						VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,now())";
+					$result = $conn -> prepare($insert_sql);
+					$result -> execute(array( $transactionId,$item["type"],$item["report_no"],$item["shape"],$item["color"],
+							$item["fancy"],$item["grading_lab"],$item["carat"], $item["clarity"],$item["cut_grade"],
+							$item["polish"], $item["symmetry"],$item["price"],$item["jewerly"], $item["material"],
+							$item["jewerly_price"]
+					));
+				}
 			}
-			$receiptId = $conn->lastInsertId();
-			echo 'ok';
+			echo $transactionId;
 			break;
 		case "currencyRate":
 			$from=$_REQUEST['from'];
@@ -382,14 +447,20 @@ if($_REQUEST['action']) {
 				$USD_GBP=$row_currency['USD_GBP'];
 				$USD_CNY=$row_currency['USD_CNY'];
 			}
-			if($from=='EUR'&&$to=='CNY') {
+			if($from=='EUR'&&$to=='CNY') 
 				$rate=($USD_CNY/$USD_EUR);
-			}
-			if($from=='CNY'&&$to=='EUR') {
+			if($from=='EUR'&&$to=='USD')
+				$rate=1/$USD_EUR;
+			if($from=='CNY'&&$to=='EUR') 
 				$rate=($USD_EUR/$USD_CNY);
-			}
+			if($from=='CNY'&&$to=='USD')
+				$rate=(1/$USD_CNY);
+			if($from=='USD'&&$to=='EUR')
+				$rate=($USD_EUR);
+			if($from=='USD'&&$to=='CNY')
+				$rate=($USD_CNY);
 			echo $rate;
-			break;
+				break;
 		case "fetchDia":
 			$ref=$_REQUEST['ref'];
 			$currency=$_REQUEST['currency'];
@@ -413,13 +484,13 @@ if($_REQUEST['action']) {
 			}
 			break;
 		case "invoiceNo":
-			$invoiceNo = 1;
-			$sql_dia='SELECT count(*) as t FROM invoice ';
+			$transactionNo = 1;
+			$sql_dia='SELECT count(*) as t FROM transaction where type="invoice"';
 			foreach($conn->query($sql_dia) as $r_r){
-				$invoiceNo=$r_r['t']+1;
+				$transactionNo=$r_r['t']+1;
 			}
-			$invoiceStr=  date('Y').sprintf('%04s', $invoiceNo);
-			echo $invoiceStr;
+			$transactionStr=  date('Y').sprintf('%04s', $transactionNo);
+			echo $transactionStr;
 			break;
 		case "appointmentMake":
 			$diaId=$_REQUEST['diaId'];
@@ -498,7 +569,7 @@ if($_REQUEST['action']) {
 			echo $OK;
 			break;
 		default:
-			echo "ok";
+			echo "wrong action";
 			break;
 	}
 }
