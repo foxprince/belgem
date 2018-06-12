e04cf7c81bfe0d19510a480e3ba0b9f7f040aa99
foxprince <9600348@qq.com>
2018-06-12 22:01:44 +0800

    得到的

--- cn/diamond-data.php
+++ cn/diamond-data.php
@@ -196,7 +196,7 @@ $conn=dbConnect('write','pdo');
 $conn->query("SET NAMES 'utf8'");
 include_once('../log.php');
 
-$sql_count='SELECT COUNT(*) AS num FROM diamonds WHERE'.$query_fromCompany.$query_shape.$query_color.$query_clarity.$query_cut.$query_polish.$query_sym.$query_fluo.$query_certi.$and.'(carat >= '.$query_weight_from.' AND carat <= '.$query_weight_to.') AND (price BETWEEN '.$query_price_from.' AND '.$query_price_to.') AND status = "AVAILABLE" '.$featured;
+$sql_count='SELECT COUNT(*) AS num FROM diamonds WHERE'.$query_shape.$query_fromCompany.$query_color.$query_clarity.$query_cut.$query_polish.$query_sym.$query_fluo.$query_certi.$and.'(carat >= '.$query_weight_from.' AND carat <= '.$query_weight_to.') AND (price BETWEEN '.$query_price_from.' AND '.$query_price_to.') AND status = "AVAILABLE" '.$featured;
 logger($query_fromCompany.$sql_count);
 if(!$superAdmin)
 	$sql_count = $sql_count.' AND visiable=1';
@@ -205,7 +205,7 @@ foreach($conn->query($sql_count) as $num){
 }
 /**/
 
-$sql='SELECT * FROM diamonds WHERE'.$query_fromCompany.$query_shape.$query_color.$query_clarity.$query_cut.$query_polish.$query_sym.$query_fluo.$query_certi.$and.'(carat >= '.$query_weight_from.' AND carat <= '.$query_weight_to.') AND (price BETWEEN '.$query_price_from.' AND '.$query_price_to.') AND status = "AVAILABLE" '.$featured;
+$sql='SELECT * FROM diamonds WHERE'.$query_shape.$query_fromCompany.$query_color.$query_clarity.$query_cut.$query_polish.$query_sym.$query_fluo.$query_certi.$and.'(carat >= '.$query_weight_from.' AND carat <= '.$query_weight_to.') AND (price BETWEEN '.$query_price_from.' AND '.$query_price_to.') AND status = "AVAILABLE" '.$featured;
 if(!$superAdmin)
 	$sql_ = $sql.' AND visiable=1';
 $sql = $sql.' '.$query_sorting.' LIMIT '.$startfrom.', 35';
