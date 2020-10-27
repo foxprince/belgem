612cc54f3bdf3eacc04f4c34f49a3815c2131f9e
foxprince <9600348@qq.com>
2020-10-27 17:24:22 +0800

    z

--- cn/content/contactinfo.php
+++ cn/content/contactinfo.php
@@ -2,12 +2,12 @@
 
 
 <div class="main_contentbox" style="font-size:14px;">
-<img class="contentpic" style="margin: 0 15px 0 0; float: left;" src="../images/site_elements/contact-img.jpg" alt="" width="388">
+<img class="contentpic" style="margin: 0 15px 0 0; float: left;" src="../images/site_elements/about-img.jpg" alt="" width="388">
 <div style="padding-right:15px;">
 <img src="../images/site_elements/getqrcode.jpg" style="position:absolute; top:15px; right:15px; width:98px;" />
 
 <h3>访问利美</h3>
-  <p>无论是现场咨询还是提货，Belgem钻石公司都欢迎您的亲临访问。
+  <p>无论是现场咨询还是提货，Belgem都欢迎您的亲临访问。
   </p>
   <p><strong style="font-size:18px;">联系方式</strong></p>
   <p>电话：+32 (0)3 689 73 94</p>
