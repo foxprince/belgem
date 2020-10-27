36e7129a1310e50c5ad043bc07eb7b90462b78e3
foxprince <9600348@qq.com>
2020-10-27 17:15:08 +0800

    z

--- cn/content/brandstory.php
+++ cn/content/brandstory.php
@@ -7,7 +7,7 @@
 <div style="height:464px; padding: 15px 15px 0 0;">
 <h3 id="antwerp" style="margin-top:0;"><span style="font-size: 12pt;">BELGEM BVBA</span></h3>
 <p><span style="font-size: 12pt;"><span style="font-size: 12pt;"></span></span></p>
-<p><br><span style="font-size: 12pt;">Jewelry products and new design</span></p>
+<p><br><span style="font-size: 12pt;">BELGEM is professional in Jewelry design and consulting service</span></p>
 <!--
 <p>利美钻石专注于<a href="https://www.lumiagem.com/about.php?p=knowledge" title="安特卫普钻石"><strong>安特卫普钻石</strong></a>和<a href="https://www.lumiagem.com/about.php?p=knowledge" title="比利时钻石">比利时钻石</a></p>
 <p>更多钻石请看这里:<a href="https://www.lumiagem.com/about.php?p=knowledge" title="钻石知识">钻石知识</a></p>
--- cn/includes/header.php
+++ cn/includes/header.php
@@ -1,13 +1,12 @@
 <div class="header">
 
-<a id="logolinker" href="/"><img id="logo" src="http://happyeurope.eu/images/belgemlogo.png" /></a>
+<a id="logolinker" href="/cn/diamonds.php"><img id="logo" src="http://happyeurope.eu/images/belgemlogo.png" /></a>
 
 
 <div id="navi">
 <ul>
 <li><a id="aboutbtn" href="about.php">品牌故事</a></li>
 
-<li><a id="diamondsbtn" href="diamonds.php">精品钻石</a></li>
 <li><a id="jewelrybtn" href="jewelry.php">精品首饰</a></li>
 
 <li><a id="contactmainbtn" href="contact.php">联系我们</a></li>
