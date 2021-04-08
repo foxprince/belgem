6bf3e03c4936d7acb6ac3c3a460b1d2ecf009ba5
foxprince <9600348@qq.com>
2021-04-09 00:17:59 +0800

    zssss

--- cn/content/contactinfo.php
+++ cn/content/contactinfo.php
@@ -4,16 +4,13 @@
 <div class="main_contentbox" style="font-size:14px;">
 <img class="contentpic" style="margin: 0 15px 0 0; float: left;" src="../images/site_elements/about-img.jpg" alt="" width="388">
 <div style="padding-right:15px;">
-<img src="../images/site_elements/getqrcode.jpg" style="position:absolute; top:15px; right:15px; width:98px;" />
 
-<h3>访问利美</h3>
-  <p>无论是现场咨询还是提货，Belgem都欢迎您的亲临访问。
+  <p>Whether it's on-site consultation or picking up, We welcomes your visit.
   </p>
-  <p><strong style="font-size:18px;">联系方式</strong></p>
-  <p>电话：+32 (0)3 689 73 94</p>
-  <p>邮箱：info@lumiagem.com</p>
-  <p>微信：请扫瞄右侧二维码</p>
-  <p>地址：Pelikaanstraat 62, 2018 Antwerp, Belgium （比利时安特卫普）<br />
+  <p><strong style="font-size:18px;">Contact US</strong></p>
+  <p>Tel：+32 (0)3 689 73 94</p>
+  <p>Email：chuxiao2008@hotmail.com</p>
+  <p>Address：Pelikaanstraat 62, 2018 Antwerp, Belgium （比利时安特卫普）<br />
     <br />
   </p>
 
